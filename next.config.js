ba11c50ea1f852548f9346651c497808f8d9e20d
Hackathon Salta <hackathon@salta.gob.ar>
2026-05-29 17:21:06 -0300

    Fix: change next.config to .js with standalone output for Vercel compatibility

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   images: {
     unoptimized: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
